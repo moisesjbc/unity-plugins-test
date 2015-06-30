10fffe9fd644ee3e4577fd44ad2c57a1bc2d7301
Moisés <moises@Mac-mini-de-Sebastian.local>
2015-06-30 12:06:24 +0100

    Added script for moving the camera forward and backwards.

---
+++ UnityProject/Assets/MoveCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		const float step = 0.01f;
+		if( Input.GetKey( KeyCode.UpArrow ) ){
+			transform.Translate( step * Vector3.forward );
+		}
+		if( Input.GetKey( KeyCode.DownArrow ) )
+		{
+			transform.Translate( -step * Vector3.forward );
+		}
+	}
+}
