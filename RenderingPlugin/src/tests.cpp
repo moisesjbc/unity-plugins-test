82010cef68eec4ebbc66a44ba43c3a5dbf13bd10
moisesjbc <moisesjbc@gmail.com>
2015-07-07 09:41:44 +0100

    Added SDL2 code for creating a testing window (tests).

--- RenderingPlugin/src/tests.cpp
+++ RenderingPlugin/src/tests.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <stdexcept>
+
+int RES_X = 400;
+int RES_Y = 300;
 
 int main( int argc, char* argv[] )
 {
-    std::cout << "Hello world!" << std::endl;
+    // Initialize the SDL library
+    if( SDL_Init( SDL_INIT_VIDEO ) ){
+        throw std::runtime_error( SDL_GetError() );
+    }
+    atexit( SDL_Quit );
+
+    SDL_Window* window_ = SDL_CreateWindow( "RenderingPlugin",
+                                SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED,
+                                RES_X,
+                                RES_Y,
+                                0 );
+    if( !window_ ){
+        throw std::runtime_error( SDL_GetError() );
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer( window_, -1, 0);
+    if( !renderer ){
+        throw std::runtime_error( SDL_GetError() );
+    }
+
+    SDL_Event event;
+    bool exitTest = false;
+    while( !exitTest ){
+        while( SDL_PollEvent( &event ) ){
+            exitTest = ( event.type == SDL_QUIT );
+        }
+        SDL_RenderClear( renderer );
+        SDL_RenderPresent( renderer );
+    }
     return 0;
 }
