7f928761f74fcafce78e28878dad8fc6acb87c23
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2015-08-13 12:53:31 +0100

    Fixed include directory.

--- RenderingPlugin/jni/Android.mk
+++ RenderingPlugin/jni/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := RenderingPlugin
 LOCAL_SRC_FILES := ../src/lod_plane.cpp ../src/RenderingPlugin.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/.../include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 
 include $(BUILD_SHARED_LIBRARY)
