67f2063aa8c31895eb0c04caba311da9e6875249
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2015-08-21 11:36:40 +0100

    Added header platform.hpp with platform specifics.

--- RenderingPlugin/include/RenderingPlugin.h
+++ RenderingPlugin/include/RenderingPlugin.h
@@ -1,47 +1,7 @@
 #ifndef RENDERING_PLUGIN_H
 #define RENDERING_PLUGIN_H
 
-// Which platform we are on?
-#if _MSC_VER || __MINGW32__
-#define UNITY_WIN 1
-#elif defined(__APPLE__)
-	#if defined(__arm__)
-		#define UNITY_IPHONE 1
-	#else
-		#define UNITY_OSX 1
-	#endif
-#elif defined(__linux__)
-#define UNITY_LINUX 1
-#elif defined(UNITY_METRO) || defined(UNITY_ANDROID)
-// these are defined externally
-#else
-#error "Unknown platform!"
-#endif
-
-
-// Attribute to make function be exported from a plugin
-#if UNITY_METRO
-#define EXPORT_API __declspec(dllexport) __stdcall
-#elif UNITY_WIN
-#define EXPORT_API __declspec(dllexport)
-#else
-#define EXPORT_API
-#endif
-
-
-// Which graphics device APIs we possibly support?
-#if UNITY_WIN
-#define SUPPORT_OPENGL 1
-#endif
-
-#if UNITY_OSX || UNITY_LINUX
-#define SUPPORT_OPENGL 1
-#endif
-
-#if UNITY_IPHONE || UNITY_ANDROID
-	#define SUPPORT_OPENGLES 1
-#endif
-
+#include <platform.hpp>
 
 // Graphics device identifiers in Unity
 enum GfxDeviceRenderer
--- RenderingPlugin/include/lod_plane.hpp
+++ RenderingPlugin/include/lod_plane.hpp
@@ -3,28 +3,7 @@
 
 #include <RenderingPlugin.h>
 
-// Include headers for the graphics APIs we support
-
-#if !SUPPORT_OPENGL
-#error "OpenGL required!"
-#endif
-
-#include <easylogging++.h>
-
-#define GL_GLEXT_PROTOTYPES
-#if !__ANDROID__ && (UNITY_WIN || UNITY_LINUX)
-    #include <windows.h>
-	#define GLEW_STATIC
-    #include <GL/glew.h>
-#elif UNITY_IPHONE
-    #include <OpenGLES/ES2/gl.h>
-#elif UNITY_ANDROID || __ANDROID__
-    #include <GLES2/gl2.h>
-#else
-    // TODO: can't include GLES2 on Mac?
-    // (http://forums.macrumors.com/threads/xcode-opengl-es2-desktop-project.1109285/)
-    #include <OpenGL/gl3.h>
-#endif
+#include <platform.hpp>
 
 #include <vector>
 
